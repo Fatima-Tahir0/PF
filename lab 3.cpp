c60701cc6a4d974ea3ef9756867ae16d8c377d03
Fatima-Tahir0 <fatima.tahir000786@gmail.com>
2024-11-03 22:07:26 +0500

    Add files via upload

---
+++ lab 3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float celsius,fahrenheit;
+	cout<<"enter the temperature in celsius"<<endl;
+	cin>>celsius;
+	fahrenheit=(celsius*9/5)+32;
+	cout<<"temperature in fahrenheit"<<fahrenheit<<endl;
+	return 0;
+}
